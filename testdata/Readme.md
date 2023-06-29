5eeb4b4359426bd43f444c05ec82daffe0b0fc8e
Bing Guo <gbinux@gmail.com>
2023-06-29 15:10:36 -0400

    update testdata

--- testdata/Readme.md
+++ testdata/Readme.md
@@ -3,6 +3,4 @@ simulated data
 1. script: `sim.py`
 2. `bcf` folder: un-filtered genotype data
 3. `vcf_filt` folder: only biallelic site with maf > 0.01
-4. `ibd1` folder: hap-ibd calls with threshold of 3 cM
-5. `ibd2` folder: hap-ibd calls with threshold of 2 cM
-6. `map` folder: plink-format map files
+4. `map` folder: plink-format map files
--- testdata/sim_sel.sh
+++ testdata/sim_sel.sh
@@ -1,4 +1,4 @@
-# clone an existing repo
+# clone repos used for simulation
 git clone git@github.com:bguo068/posseleff_simulations.git
 git -C posseleff_simulations checkout c58f9d988c9827ce870f859fa1230a0679fc4e32
 
@@ -12,10 +12,13 @@ cd ..
 # install hmmibd that allows to change recombination rate
 git clone git@github.com:bguo068/hmmibd-rs.git
 cd hmmibd-rs
+git checkout rs # rs branch has improved hmmibd2 code
 x86_64-conda-linux-gnu-gcc -o hmmIBD_rec -O3 -lm -Wall hmmIBD.c
+cargo build --release --example hmmibd2
 cd ..
 cp hmmibd-rs/hmmIBD_rec  posseleff_simulations/bin/
-
+cp hmmibd-rs/target/release/examples/hmmibd2 posseleff_simulations/bin/
+rm -rf hmmibd-rs
 
 # modified some code to allow output heterzygous diploid vcf files
 sed -i  's/write_peudo_homozygous_vcf(simulator/# write_peudo_homozygous_vcf(simulator/' posseleff_simulations/bin/sim_single_pop.py
@@ -24,34 +27,37 @@ echo '
         simulator.mutated_trees.write_vcf(f, contig_id=f"chr{args.chrno}")
 ' >>  posseleff_simulations/bin/sim_single_pop.py
 
+# download hapibd jar file
+wget https://faculty.washington.edu/browning/hap-ibd.jar
+
 # simulation and get tree/vcf files
 for chrno in {1..3}; do 
     echo $chrno
     posseleff_simulations/bin/sim_single_pop.py \
         --chrno $chrno \
-        --seqlen 100000000 \
-        --selpos 30000000 \
+        --seqlen 1500000 \
+        --selpos  500000 \
         --num_origins 1 \
         --N 10000 \
         --s 0.3 \
         --h 0.5 \
         --g_sel_start 80 \
-        --r 1e-8 \
+        --r 6.6666667e-7 \
         --sim_relatedness 0 \
         --g_ne_change_start 200 \
-        --N0 2000 \
+        --N0 1000 \
         --u 1e-8 \
-        --nsam 2000 \
+        --nsam 1000 \
         --genome_set_id 03
 done
 
 if [ ! -d ibd_tskibd ]; then mkdir ibd_tskibd; fi
 if [ ! -d ibd_hapibd ]; then mkdir ibd_hapibd; fi
 if [ ! -d ibd_hmmibd ]; then mkdir ibd_hmmibd; fi
+if [ ! -d ibd_hmmibd2 ]; then mkdir ibd_hmmibd2; fi
 if [ ! -d map ]; then mkdir map; fi
 if [ ! -d bcf ]; then mkdir bcf; fi
 if [ ! -d vcf_filt ]; then mkdir vcf_filt; fi
-wget https://faculty.washington.edu/browning/hap-ibd.jar
 
 for chrno in {1..3}; do 
     bcftools view 3_${chrno}.vcf -Ob -o bcf/sel_chr${chrno}.bcf
@@ -62,21 +68,22 @@ for chrno in {1..3}; do
     bcftools index -f vcf_filt/sel_chr${chrno}.vcf.gz
 
     # ## call tskibd
-    posseleff_simulations/bin/tskibd ${chrno} 1000000 10000 2 3_${chrno}.trees
-    mv ${chrno}.ibd > ibd_tskibd/
     mv 3_${chrno}.trees ibd_tskibd/
+    posseleff_simulations/bin/tskibd ${chrno} 15000 150 2 ibd_tskibd/3_${chrno}.trees
+    mv ${chrno}.ibd ibd_tskibd/
+    echo "chr${chrno} . 0.0 1" > ${chrno}.map
+    echo "chr${chrno} . 100.0 1500000" >> ${chrno}.map
     mv ${chrno}.map map/chr${chrno}.map
 
     # ## call hapibd
     java -jar hap-ibd.jar gt=vcf_filt/sel_chr${chrno}.vcf.gz map=map/chr${chrno}.map min-output=2 out=ibd_hapibd/chr${chrno}
 
     # convert file
-    bcftools query -f "%CHROM\t%POS[\t%GT]\n" vcf_filt/sel_chr${chrno}.vcf.gz | tr '|' '\t' | sed 's/^chr//' > hmmibdinput.txt
-    ./posseleff_simulations/bin/hmmIBD_rec -i hmmibdinput.txt -n 100 -r 1e-8 -o ./ibd_hmmibd/$chrno
-
+    echo | awk -v OFS='\t' 'END{$1="CHROM"; $2 = "POS"; for (i=0; i<1000; i++) $(3+i)=i; print}' > hmmibdinput.txt
+    bcftools query -f "%CHROM\t%POS[\t%GT]\n" vcf_filt/sel_chr${chrno}.vcf.gz | tr '|' '\t' | sed 's/^chr//' >> hmmibdinput.txt
+    # ./posseleff_simulations/bin/hmmIBD_rec -i hmmibdinput.txt -n 100 -r  6.6666667e-7 -o ./ibd_hmmibd/$chrno
+    ./posseleff_simulations/bin/hmmibd2 -i hmmibdinput.txt -n 100 --rec-rate  6.6666667e-7 --max-all 2 --filt-ibd-only  --filt-min-seg-cm 2.0 -o ./ibd_hmmibd2/$chrno
 done
 
-
-
-
-rm 3_* tmp* *.log *.map
+# clear intermediate files
+rm 1* 2* 3*
